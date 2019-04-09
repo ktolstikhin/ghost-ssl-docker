7b4d92a6d6d4abbf1416e4fb4798cb44942e8bff
Konstantin Tolstikhin <k.tolstikhin@gmail.com>
2019-04-09 09:46:57 +0800

    fix bug

--- cron/bin/backup.sh
+++ cron/bin/backup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 source $(dirname "$0")/.ghost_blog_env
-period={1:-daily}
+period=${1:-daily}
 
 if [ $period = daily ]; then
   timestamp=$(date +\%A)
@@ -12,7 +12,7 @@ fi
 backup_archive=${BACKUP_PATH}/ghost_backup_${timestamp}.tar.gz
 echo "$(date) [INFO] Backup $DATA_PATH to $backup_archive"
 
-tar -zcvf $backup_archive $DATA_PATH
+tar -zcf $backup_archive $DATA_PATH
 
 if [ $? -ne 0 ]; then
   echo "$(date) [ERROR] Backup failed!"
