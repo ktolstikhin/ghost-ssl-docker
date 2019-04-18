cc2ca123e689c964f905f326cbf4534b5c533481
Konstantin Tolstikhin <k.tolstikhin@gmail.com>
2019-04-18 21:54:59 +0800

    refactor & clean up

--- cron/bin/backup.sh
+++ cron/bin/backup.sh
@@ -2,23 +2,23 @@
 
 period=${1:-daily}
 
-if [ $period = daily ]; then
+if [ ${period} = daily ]; then
   timestamp=$(date +\%A)
-elif [ $period = weekly ]; then
+elif [ ${period} = weekly ]; then
   timestamp=$(date -I)
 else
-  echo "[ERROR] Unsupported backup period!"
+  echo "$(date) [ERROR] Unsupported backup period!"
   exit 1
 fi
 
 data=${DATA_PATH}/data
 archive=${DATA_PATH}/backup/ghost_backup_${timestamp}.tar.gz
 
-echo "[INFO] Backup $data to $archive"
-tar -zcf $archive $data
+echo "$(date) [INFO] Backup ${data} to ${archive}"
+tar -zcf ${archive} ${data}
 
 if [ $? -ne 0 ]; then
-  echo "[ERROR] Backup failed!"
+  echo "$(date) [ERROR] Backup failed!"
   exit 1
 fi
 
